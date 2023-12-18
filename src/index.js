4623386ae7a13f13a38db70d9935e68a73964fbd
mariavsl518 <mariavsl518@gmail.com>
2023-12-18 21:30:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="mariavsl518-goit-react-hw-01-components">
     <App />
   </React.StrictMode>
 );
